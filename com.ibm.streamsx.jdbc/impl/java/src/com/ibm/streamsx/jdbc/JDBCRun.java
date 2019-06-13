199c80793e634688d6bb876c2fdfd4826b2b28d4
anouri <anouri@sbln7.ibm.com>
2019-06-13 18:37:47 +0200

    check the value of idleSessionTimeOut

--- com.ibm.streamsx.jdbc/impl/java/src/com/ibm/streamsx/jdbc/JDBCRun.java
+++ com.ibm.streamsx.jdbc/impl/java/src/com/ibm/streamsx/jdbc/JDBCRun.java
@@ -483,7 +483,7 @@ public class JDBCRun extends AbstractJDBCOperator {
 			startCheckConnection(context);
 		}
 
-		if (idleSessionTimeOutMinute > 1) {
+		if (idleSessionTimeOutMinute > 0) {
 			startidleSessionTimeOutThread(context);
 		}
 		
@@ -583,7 +583,7 @@ public class JDBCRun extends AbstractJDBCOperator {
 				{
 				    Thread.sleep(60000);
 				    idleSessionTimeOutTimer ++;
- //                   System.out.println("idleSessionTimeOut " + idleSessionTimeOutMinute + " idleSessionTimeOutTimer " + idleSessionTimeOutTimer);
+                    // System.out.println("idleSessionTimeOutThread " + idleSessionTimeOutMinute + " idleSessionTimeOutTimer " + idleSessionTimeOutTimer);
 				} 
 				catch(InterruptedException ex) 
 				{
@@ -592,7 +592,7 @@ public class JDBCRun extends AbstractJDBCOperator {
 
 				try 
 				{
-					if (idleSessionTimeOutTimer > idleSessionTimeOutMinute){
+					if (idleSessionTimeOutTimer >= idleSessionTimeOutMinute){
 				    	
 						if (jdbcClientHelper.isValidConnection()) {	
 							try 
@@ -601,7 +601,9 @@ public class JDBCRun extends AbstractJDBCOperator {
 								// close the connection
 							    Thread.sleep(1000);
 								jdbcClientHelper.closeConnection();
-			                    System.out.println("close connection idleSessionTimeOut " + idleSessionTimeOutMinute + " idleSessionTimeOutTimer " + idleSessionTimeOutTimer);
+								if (!jdbcClientHelper.isValidConnection()) {	
+									System.out.println("The idleSessionTimeOut closed the current JDBC connection after " + idleSessionTimeOutMinute  + " minutes ");
+								}
 							}
 					catch (Exception e2) {
 						e2.printStackTrace();													
